7368ff7fb3403beba17e0eadd6d5c4ccdee96594
Programmer2753 <143874664+Programmer2753@users.noreply.github.com>
2026-02-24 11:26:42 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', () => {
+function lang_menu() {
     const select = document.getElementById('language');
     const wrapper = document.querySelector('.language-selection');
 
@@ -18,4 +18,34 @@ document.addEventListener('DOMContentLoaded', () => {
     select.addEventListener('blur', () => {
         wrapper.classList.remove('open');
     });
+}
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const catalogue_button = document.querySelector('.catalogue-btn');
+    const catalogue_menu = document.querySelector('.catalogue-menu');
+    const overlay = document.querySelector('.menu-overlay');
+
+    if (!catalogue_button || !catalogue_menu || !overlay) return;
+
+
+    catalogue_button.addEventListener('click', (e) => {
+        e.preventDefault();
+        catalogue_button.classList.toggle('open');
+        catalogue_menu.classList.toggle('open');
+        overlay.classList.toggle('open');
+    });
+
+    document.addEventListener('click', (e) => {
+        const isClickInsideMenu = catalogue_menu.contains(e.target);
+        const isClickOnButton = catalogue_button.contains(e.target);
+
+        if (!isClickInsideMenu && !isClickOnButton) {
+            catalogue_menu.classList.remove('open');
+            catalogue_button.classList.remove('open');
+            overlay.classList.remove('open');
+        }
+    });
+    
+    lang_menu();
 });
